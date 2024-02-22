f0fcd64c4dcb7f3fafaf6898cc4ca921c2e9506c
Darshil <darsgajul2003@gmail.com>
2024-02-23 03:17:02 +0530

    donechange

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,11 @@ import EditPost from "./_root/pages/EditPost";
 import PostDetails from "./_root/pages/PostDetails";
 import Profile from "./_root/pages/Profile";
 import UpdateProfile from "./_root/pages/UpdateProfile";
-import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <main className="flex h-screen">
-      <ScrollToTop />
+      {/* <ScrollToTop /> */}
       <Routes>
         {/* Public Route */}
         <Route element={<AuthLayout />}>
