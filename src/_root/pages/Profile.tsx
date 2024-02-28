c035328b036052f88a628cff2f7164d23c1e81d4
Darshil <darsgajul2003@gmail.com>
2024-02-28 18:21:49 +0530

    button styling

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -60,16 +60,15 @@ const Profile = () => {
                 {currentUser.name}
               </p>
             </div>
+            <p className="small-medium md:base-medium text-center xl:text-left mt-2 max-w-screen-sm">
+              {currentUser.bio}
+            </p>
 
             <div className="flex gap-8 mt-4 items-center justify-center xl:justify-start flex-wrap z-20">
               <StatBlock value={currentUser.posts.length} label="Posts" />
               <StatBlock value={20} label="Followers" />
               <StatBlock value={20} label="Following" />
             </div>
-
-            <p className="small-medium md:base-medium text-center xl:text-left mt-2 max-w-screen-sm">
-              {currentUser.bio}
-            </p>
           </div>
 
           <div className="flex justify-center gap-4">
