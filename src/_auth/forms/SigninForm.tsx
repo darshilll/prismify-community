a02492b78414fb523419733ab218dac1377676cc
Darshil <darsgajul2003@gmail.com>
2024-03-06 17:37:24 +0530

    signin

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -84,7 +84,7 @@ const SigninForm = () => {
                     {...field}
                   />
                 </FormControl>
-                <FormMessage className="shad-form_message"/>
+                <FormMessage className="shad-form_message" />
               </FormItem>
             )}
           />
@@ -103,7 +103,7 @@ const SigninForm = () => {
                     {...field}
                   />
                 </FormControl>
-                <FormMessage className="shad-form_message"/>
+                <FormMessage className="shad-form_message" />
               </FormItem>
             )}
           />
